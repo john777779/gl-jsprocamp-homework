bed9661589e3b19bf79028d721b67da700c6fd44
Ievgen Shmulia <john77777@ukr.net>
2018-02-01 12:40:56 +0200

    HW1 and HW2

--- src/lesson1/task.js
+++ src/lesson1/task.js
@@ -2,7 +2,7 @@
   Напишите функцию, которая принимает 1 аргумент и возварщает его тип
 */
 function getDataType(variable) {
-
+  return typeof variable;
 }
 
 /*
@@ -14,7 +14,19 @@ function getDataType(variable) {
   'object-function' - если функция
 */
 function getDataTypePseudoName(variable) {
-
+  const typeVariable = typeof variable;
+  if (typeVariable === 'boolean' || typeVariable === 'number' || typeVariable === 'string') {
+    return 'primitive';
+  } else if (typeVariable === 'undefined' || variable === null) {
+    return 'primitive-special';
+  } else if (typeVariable === 'function') {
+    return 'object-function';
+  } else if (typeVariable === 'object' ) {
+      if (Array.isArray(variable)) {
+        return 'object-array';
+      }
+    return 'object';
+  }
 }
 
 
@@ -25,7 +37,13 @@ function getDataTypePseudoName(variable) {
   и -1 в другом случае
 */
 function compareByType(a, b) {
-
+  if (a === b) {
+  		return 1;
+  	} else if (a == b) {
+  		return 0;
+  	} else {
+  		return -1;
+  	}
 }
 
 // Numbers
@@ -37,15 +55,23 @@ function compareByType(a, b) {
   в любом другом случае возврвщвет -1
 */
 function increase(value) {
-
+  if (typeof value === 'number') {
+		return value + 1;
+	} else {
+		return -1;
+	}
 }
 
 /*
   Напишите функцию, которая принимает 1 аргумент(число),
-  и в случае если аргумент не Infinity или NaN возвращает строку 'safe' иначе 'danger'
+  и в случае если аргумент не Infinity или NaN возвращает строку 'safe'
+  иначе 'danger'
 */
 function testForSafeNumber(value) {
-
+  if (Number.isFinite(value)) {
+    return 'safe';
+  }
+  return 'danger';
 }
 
 
@@ -57,7 +83,7 @@ function testForSafeNumber(value) {
   и возвращает массив из елементов строки разделенных по пробелу ' '
 */
 function stringToArray(str) {
-
+  return str.split(' ');
 }
 
 
@@ -66,35 +92,47 @@ function stringToArray(str) {
   и возвращает часть этой строки до первой запятой
 */
 function getStringPart(str) {
-
+  if (typeof str === 'string') {
+    return str.split(',')[0];
+  }
+  return 'Error argument type';
 }
 
 /*
   Напишите функцию, которая принимает 2 аргумента (строку и симовл),
-  и возвращает порядковый номер симовола в строе если символ встречается в строке 1 раз,
+  и возвращает порядковый номер симовола в строе если символ встречается
+  в строке 1 раз,
   false в противоположном случае
 */
 function isSingleSymbolMatch(str, symbol) {
-
+  if (str.indexOf(symbol) === str.lastIndexOf(symbol)) {
+    return str.indexOf(symbol);
+  }
+  return false;
 }
 
 /*
   Напишите функцию, которая принимает 2 аргумента,
   массив в разделитель[опционально],
-  и возвращает строку ввиде элементов массива c разделителями если разделитель задан
+  и возвращает строку ввиде элементов массива c разделителями если
+  разделитель задан
   или строку разделенную "-" если не задан
 */
 function join(array, separator) {
-
+  if (separator) {
+    return array.join(separator);
+  }
+  return array.join('-');
 }
 
 
 /*
   Напишите функцию, которая принимает 2 массива,
-  и возвращает один состоящий из элементов перового и второго (последовательно сначала первый потом второй)
+  и возвращает один состоящий из элементов перового и второго
+  (последовательно сначала первый потом второй)
 */
 function glue(arrA, arrB) {
-
+  return arrA.concat(arrB);
 }
 
 
@@ -103,7 +141,13 @@ function glue(arrA, arrB) {
   и возвращает другой массив отсортированный от большего к меньшему
 */
 function order(arr) {
-
+  const sortArr = arr.sort((a, b) => {
+    if (a !== b) {
+      return b > a ? 1 : -1;
+    }
+    return 0;
+  })
+  return sortArr;
 }
 
 
@@ -112,7 +156,13 @@ function order(arr) {
   и возвращает другой без чисел которые меньше 0
 */
 function removeNegative(arr) {
-
+  for (let i = 0; i < arr.length; i++) {
+		if (arr[i] < 0) {
+			arr.splice(i, 1);
+      i--;
+		};
+	};
+	return arr;
 }
 
 /*
@@ -122,7 +172,7 @@ function removeNegative(arr) {
   [1,2,3], [1, 3] => [2]
 */
 function without(arrA, arrB) {
-
+  return arrA.filter(x => !arrB.includes(x));
 }
 
 /*
@@ -133,7 +183,12 @@ function without(arrA, arrB) {
   '12/6' => 2
 */
 function calcExpression(expression) {
-
+  try {
+    return eval(expression);
+  }
+  catch (error) {
+    return NaN;
+  }
 }
 
 /*
@@ -145,7 +200,12 @@ function calcExpression(expression) {
   '100>5' => true
 */
 function calcComparison(expression) {
-
+  try {
+    return eval(expression);
+  }
+  catch (error) {
+    throw error;
+  }
 }
 
 /*
@@ -157,7 +217,20 @@ function calcComparison(expression) {
   { a: 1, b: 2 }, '.c' => exception
 */
 function evalKey(obj, expression) {
-
+  let result = obj;
+  const arrExp = expression.split('.');
+  if (arrExp.length == 1) {
+    throw new Error();
+  }
+  for (const el of arrExp) {
+    if (el) {
+      result = result[el];
+    }
+  }
+  if (result == undefined) {
+    throw new Error();
+  }
+  return result;
 }
 
 export default {
