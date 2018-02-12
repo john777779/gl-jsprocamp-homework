4378286c2450abbaecf06aef05a87e02a3fe663e
Ievgen Shmulia <john77777@ukr.net>
2018-02-12 11:54:31 +0200

    Added HW4

--- src/lesson4/task.js
+++ src/lesson4/task.js
@@ -8,8 +8,43 @@
   in forEach() method.
   mySet = createSet(['a', 'b', 'c'])
 */
-export function createSet(arr) {
-
+export function createSet(arr = []) {
+  const setPrototype = {
+    get size() {
+      return this.length || 0;
+    },
+    add(val) {
+      if (!Array.prototype.includes.call(this, val)) {
+        Array.prototype.push.call(this, val);
+      }
+    },
+    has(val) {
+      return Array.prototype.includes.call(this, val);
+    },
+    delete(val) {
+      if (Array.prototype.includes.call(this, val)) {
+        Array.prototype.splice.call(this, (Array.prototype.indexOf.call(this, val)), 1);
+        return true;
+      }
+      return false;
+    },
+    forEach(fn) {
+      return Array.prototype.forEach.call(this, element => fn(element));
+    },
+    clear() {
+      Array.prototype.splice.call(this, 0);
+    }
+  }
+  const arrSet = [];
+  if (arr && Array.isArray(arr)) {
+    arr.forEach(element => {
+      if (!arrSet.includes(element)) {
+        arrSet.push(element);
+      }
+    });
+    Object.setPrototypeOf(arrSet, setPrototype)
+  }
+  return arrSet;
 }
 
 /*
@@ -22,8 +57,65 @@ export function createSet(arr) {
   in forEach() method.
   myMap = createMap([['a', 1], ['b', 2], ['c', 3]])
 */
-export function createMap(arr) {
-
+export function createMap(arr = []) {
+  const mapPrototype = {
+    get size() {
+      return this.length || 0;
+    },
+    has(key) {
+      return Array.prototype.filter.call(this, (element => element[0] === key)).length > 0;
+    },
+    set(...rest) {
+      if (rest) {
+        try {
+          Array.prototype.filter.call(this, (elem => elem[0] === rest[0]))[0][1] = rest[1];
+        } catch(e) {
+          Array.prototype.push.call(this, [rest[0], rest[1]]);
+        }
+      } else {
+        try {
+          Array.prototype.filter.call(this, (elem => elem[0] === undefined))[0][1] = undefined;
+        } catch(e) {
+          if (e instanceof ReferenceError) {
+            Array.prototype.push.call(this, [undefined, undefined]);
+          }
+        }
+      }
+    },
+    delete(val) {
+      if (val) {
+        try {
+          Array.prototype.filter.call(this, elem => {
+            if (elem[0] === val) {
+              Array.prototype.splice.call(this, (Array.prototype.indexOf.call(this, elem)), 1);
+              return true;
+            }
+          })
+        } catch(e) {
+          return false;
+        }
+      }
+      return false;
+    },
+    clear() {
+      Array.prototype.splice.call(this, 0);
+    },
+    forEach(fn) {
+      return Array.prototype.forEach.call(this, element => fn(element[1], element[0]));
+    }
+  };
+  const arrMap = [];
+  if (arr && Array.isArray(arr)) {
+    arr.forEach(arrElement => {
+      try {
+        arrMap.filter(elem => elem[0] === arrElement[0])[0][1] = arrElement[1];
+      } catch (e) {
+        arrMap.push(arrElement);
+      }
+    });
+  }
+  Object.setPrototypeOf(arrMap, mapPrototype);
+  return arrMap;
 }
 
 export default {
